e23bc9dffd3984fb591c1aefa050f05383a29465
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-20 21:51:30 -0600

    Add deck class test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -15,22 +15,25 @@ class DeckTest <Minitest::Test
     assert_instance_of Deck, deck
   end
 
-#   def test_rank_of_card_at
-#     skip
-#     card1 = Card.new(:diamond, 'Queen', 12)
-#     card2 = Card.new(:spade, '3', 3)
-#     card3 = Card.new(:heart, 'Ace', 14)
-#     cards = [card1, card2, card3]
-#     deck = Deck.new(cards)
-# #somehow get one or two?
-#     assert_equal [] deck.rank_of_card_at
-#   end
+  def test_rank_of_card_at
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 3, deck.rank_of_card_at(1)
+    assert_equal 14, deck.rank_of_card_at(2)
+  end
 
   def test_high_ranking_cards
      card1 = Card.new(:diamond, 'Queen', 12)
      card2 = Card.new(:spade, '3', 3)
      card3 = Card.new(:heart, 'Ace', 14)
      cards = [card1, card2, card3]
+
      deck = Deck.new(cards)
 
    assert_equal [card1, card3], deck.high_ranking_cards
