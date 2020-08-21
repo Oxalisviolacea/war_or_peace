c17f394972623047648e81f43009bdbd87ff2aca
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-20 21:50:20 -0600

    Add deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,5 +4,40 @@ class Deck
 
   def initialize(cards)
     @cards = cards
+    @high_ranking_cards = []
+  end
+
+  def rank_of_card_at(position)
+   @cards[position].rank
+  end
+
+  def high_ranking_cards
+    return @high_ranking_cards unless @high_ranking_cards.empty?
+    @cards.each do |card|
+      if card.rank > 11
+        @high_ranking_cards << card
+      end
+    end
+    @high_ranking_cards
+  end
+
+  def high_ranking_cards
+    return @high_ranking_cards unless @high_ranking_cards.empty?
+    @cards.select do |card|
+      card.rank > 11
+    end
+  end
+
+  def percent_high_ranking
+    pct_high_ranking = (count.to_f/@cards.count.to_f)*100
+    pct_high_ranking
+  end
+
+  def remove_card
+    @cards.drop(1)
+  end
+
+  def add_card
+    @cards << cards
   end
 end
