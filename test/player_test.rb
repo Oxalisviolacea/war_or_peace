34076094fd53ee1a207a5fe2c37375fc87c5a0e9
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-20 21:53:52 -0600

    Add player class test

--- test/player_test.rb
+++ test/player_test.rb
@@ -15,4 +15,21 @@ class PlayerTest <Minitest::Test
 
    assert_instance_of Player, player
   end
+
+  def test_has_lost_can_be_false
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    deck = Deck.new([card1, card2, card3])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, deck.cards.empty?
+  end
+
+  def test_has_lost_can_be_true
+    deck = Deck.new([])
+    player = Player.new('Clarisa', deck)
+
+    assert_equal true, deck.cards.empty?
+  end
 end
