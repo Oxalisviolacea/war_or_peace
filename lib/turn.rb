24425d7922809f64d60ff45e1e2e46aa7f9dc406
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-23 17:37:19 -0600

    Update turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,10 +38,8 @@ class Turn
 
   def pile_cards
     if type == :basic
-      @spoils_of_war << @player1.deck.cards[0]
-      @spoils_of_war << @player2.deck.cards[0]
-      @player1.deck.remove_card
-      @player2.deck.remove_card
+      @spoils_of_war << @player1.deck.remove_card
+      @spoils_of_war << @player2.deck.remove_card
     elsif type == :war
       @spoils_of_war << @player1.deck.cards[0..2]
       @spoils_of_war << @player2.deck.cards[0..2]
