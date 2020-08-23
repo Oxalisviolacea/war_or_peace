50d625587bec532f2e8f7882a27ef85dbc3b54c2
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-23 15:30:25 -0600

    Update runner file

--- war_or_peace.rb
+++ war_or_peace.rb
@@ -1,9 +1,8 @@
+require "./lib/deck"
+require "./lib/card"
+require './lib/player'
+require './lib/turn'
+require './lib/play'
 
-# QUESTIONS
-# couldn't enter pry session at the end of pile_cards
-# symbols are being stored in a method?
-# basic: test_award_spoils + and concat methods doesn't work; is there a way not to write out the array?
-# mad: test
-#basic: test_pile_of_cards assert_equal explained to me last night, but I don't get it.
-# war: test_pile cards- what's the best way to create the instance variable?
-# war: test_pile cards- there are still too many cards in player1's deck.
+play = Play.new
+play.start
