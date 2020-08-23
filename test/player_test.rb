dd33de1a1157d5a5e282c503d10d7a2f0960106d
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-23 17:39:42 -0600

    Update player class

--- test/player_test.rb
+++ test/player_test.rb
@@ -7,16 +7,13 @@ require './lib/player'
 class PlayerTest <Minitest::Test
 
   def test_it_exists
-   card1 = Card.new(:diamond, 'Queen', 12)
-   card2 = Card.new(:spade, '3', 3)
-   card3 = Card.new(:heart, 'Ace', 14)
-   deck = Deck.new([card1, card2, card3])
-   player = Player.new('Clarisa', deck)
+   play = Play.new
 
    assert_instance_of Player, player
   end
 
   def test_has_lost_can_be_false
+    skip
     card1 = Card.new(:diamond, 'Queen', 12)
     card2 = Card.new(:spade, '3', 3)
     card3 = Card.new(:heart, 'Ace', 14)
