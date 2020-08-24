72529943edf43a2431ecd35edfa18877999fa7f9
Hanna Davis <hanna.e.davis@gmail.com>
2020-08-23 18:24:42 -0600

    Update player test

--- test/player_test.rb
+++ test/player_test.rb
@@ -12,23 +12,41 @@ class PlayerTest <Minitest::Test
 
     assert_instance_of Play, play
   end
-end
 
-#
-# def test_has_lost_can_be_false
-#   skip
-#   card1 = Card.new(:diamond, 'Queen', 12)
-#   card2 = Card.new(:spade, '3', 3)
-#   card3 = Card.new(:heart, 'Ace', 14)
-#   deck = Deck.new([card1, card2, card3])
-#   player = Player.new('Clarisa', deck)
-#
-#   assert_equal false, deck.cards.empty?
-# end
-#
-# def test_has_lost_can_be_true
-#   deck = Deck.new([])
-#   player = Player.new('Clarisa', deck)
-#
-#   assert_equal true, deck.cards.empty?
-# end
+  def test_if_it_has_attributes
+    play = Play.new
+    play.start
+
+    assert_equal Play.create_52_card_deck, Play.cards_52
+  end
+
+  def test_cards_shuffled
+
+   assert_not_equal cards_52[0..25], player1.card.deck
+  end
+
+  def test_deal_cards
+    assert_equal 26, player1.deck.cards.count
+    assert_equal 26, player2.deck.cards.count
+  end
+
+  def test_intro_text
+   assert_equal "Welcome to War! (or Peace) This game will be played with 52 cards.
+   The players today are Megan and Aurora.
+   Type 'GO' to start the game!
+   ------------------------------------------------------------------", Play.intro_text
+  end
+
+  def test_players
+    assert_equal player1,
+  end
+
+  def test_prompt_for_start
+  end
+
+  def test_game_loop
+  end
+
+  def test_state_winner
+  end
+end
